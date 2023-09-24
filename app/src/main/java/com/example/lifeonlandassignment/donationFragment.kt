fd5e3b4a0a5798adc500a3634c9174334e27e2e4
USER <ernestangy@gmail.com>
2023-09-24 16:39:16 +0800

    initial commit

--- app/src/main/java/com/example/lifeonlandassignment/donationFragment.kt
+++ app/src/main/java/com/example/lifeonlandassignment/donationFragment.kt
@@ -1,4 +1,57 @@
 package com.example.lifeonlandassignment
 
-class donationFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+
+class DonationFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.donation_screen, container, false)
+
+        // Initialize Spinner
+        val spinner: Spinner = view.findViewById(R.id.spinner)
+
+        // Initialize Adapter
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_data,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Set Adapter to Spinner
+        spinner.adapter = adapter
+
+        // Item Selection Handling
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                // Do something with selectedItem
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Handle case where nothing is selected
+            }
+        }
+
+        // Initialize Back Button
+        val backButton: ImageView = view.findViewById(R.id.backButtonDonation)
+
+        // Set Click Listener for Back Button
+        backButton.setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+
+        return view
+    }
 }
--- app/src/main/java/com/example/lifeonlandassignment/notificationFragment.kt
+++ app/src/main/java/com/example/lifeonlandassignment/notificationFragment.kt
@@ -9,8 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RemoteViews
+import android.widget.TableRow
 import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
+import com.example.lifeonlandassignment.admin.ClientDataFragment
+import com.example.lifeonlandassignment.admin.DonationDataFragment
+import com.example.lifeonlandassignment.admin.EventDataFragment
+import com.example.lifeonlandassignment.admin.ReportDataFragment
 
 class NotificationFragment : Fragment() {
 
@@ -20,6 +25,43 @@ class NotificationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.notification_screen, container, false)
+
+        val tableLayout1 : TableRow = view.findViewById(R.id.btnNotice1)
+        tableLayout1.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, NotificationDescriptionFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        val tableLayout2 : TableRow = view.findViewById(R.id.btnNotice2)
+        tableLayout2.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, NotificationDescriptionFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        val tableLayout3 : TableRow = view.findViewById(R.id.btnNotice3)
+        tableLayout3.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, NotificationDescriptionFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        val tableLayout4 : TableRow = view.findViewById(R.id.btnNotice4)
+        tableLayout4.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, NotificationDescriptionFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
         return view
     }
 }
